e8e01099b3d5ace4474769d1d1bc41892c39af28
lim <www.limsp95@gmail.com>
2020-12-29 04:47:48 +0800

    Added now,json

--- src/server/user/user.controller.ts
+++ src/server/user/user.controller.ts
@@ -1,28 +1,26 @@
-import { Controller , Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { User } from './user.entity';
-import { UserService} from './user.service';
-import { Post,Put, Delete, Body, Param } from  '@nestjs/common';
-
-
+import { UserService } from './user.service';
+import { Post, Put, Delete, Body, Param } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
-	constructor(private userService: UserService){}
-	
-	@Get()
-    index(): Promise<User[]> {
-      return this.userService.findAll();
-    }
+  constructor(private userService: UserService) {}
 
-    @Post('create')
-    async create(@Body() userData: User): Promise<any> {
-      return this.userService.create(userData);
-    }  
+  @Get()
+  index(): Promise<User[]> {
+    return this.userService.findAll();
+  }
 
-    @Put(':id/update')
-    async update(@Param('id') id, @Body() userData: User): Promise<any> {
-        userData.id = Number(id);
-        console.log('Update #' + userData.id)
-        return this.userService.update(userData);
-    }     
+  @Post('create')
+  async create(@Body() userData: User): Promise<any> {
+    return this.userService.create(userData);
+  }
+
+  @Put(':id/update')
+  async update(@Param('id') id, @Body() userData: User): Promise<any> {
+    userData.id = Number(id);
+    console.log('Update #' + userData.id);
+    return this.userService.update(userData);
+  }
 }
--- src/server/user/user.service.ts
+++ src/server/user/user.service.ts
@@ -2,29 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { UpdateResult, DeleteResult } from  'typeorm';
+import { UpdateResult, DeleteResult } from 'typeorm';
 
 @Injectable()
 export class UserService {
-	constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
-    ) { }
-    async  findAll(): Promise<User[]> {
-        return await this.userRepository.find();
-    }
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
 
-    async  create(user: User): Promise<User> {
-        return await this.userRepository.save(user);
-    }
+  async create(user: User): Promise<User> {
+    return await this.userRepository.save(user);
+  }
 
-    async update(user: User): Promise<UpdateResult> {
-        return await this.userRepository.update(user.id, user);
-    }
+  async update(user: User): Promise<UpdateResult> {
+    return await this.userRepository.update(user.id, user);
+  }
 
-    async delete(id): Promise<DeleteResult> {
-        return await this.userRepository.delete(id);
-    }
+  async delete(id): Promise<DeleteResult> {
+    return await this.userRepository.delete(id);
+  }
 }
-
-
